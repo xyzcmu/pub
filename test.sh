b957461ea47dcfb125d95020f6851e443c077930
xyzcmu <49193694+xyzcmu@users.noreply.github.com>
2020-03-12 11:20:38 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env bash
+
+echo "测试换行符是否影响脚本运行"
+
+read -p "是否执行更新 [y/n]" isExe
+
+if [[ $isExe == "y" ]];then
+echo "执行更新....."
+apt-get update && apt-get upgrade
+else
+echo "不更新..."
+fi
+echo "通过vscode修改过..."
+exit 0
