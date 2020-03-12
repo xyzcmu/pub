ee5a22af8e0da2c18aacbe779c63c7a619bb1cfb
xyzcmu <49193694+xyzcmu@users.noreply.github.com>
2020-03-12 11:06:39 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env bash
+
+echo "测试换行符是否影响脚本运行"
+
+read -p "是否执行更新 [y/n]" isExe
+
+if [[ $isExe == "y" ]];then
+echo "执行更新....."
+apt-get update && apt-get upgrade
+else
+echo "不更新..."
+fi
+exit 0
