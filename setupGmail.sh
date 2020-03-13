4f4a2f07d3e67460250508ed284ef166d044f086
xyzcmu <49193694+xyzcmu@users.noreply.github.com>
2020-03-13 23:49:08 +0800

    Update setupGmail.sh

--- setupGmail.sh
+++ setupGmail.sh
@@ -35,7 +35,7 @@ else
   set smtp-auth-password=$your_code
   set ssl-verify=ignore
   set nss-config-dir=/etc/pki/nssdb/
-  EOF
+EOF
   [[ $? -eq 0 ]] && echoColor 33 "ok...." || echo 34 "error...";exit 1
   echoColor 34 "s-nail 安装完成!"
   echoColor 34 "一般用法:　s-nail -s 主题 -a /path/some.file xxx@email.com < /path/some.file"
