3d23c00314fcc80c2ccf411d946459f89875e64b
xyzcmu <49193694+xyzcmu@users.noreply.github.com>
2020-07-21 13:47:39 +0800

    Update installDebian9.sh

--- installDebian9.sh
+++ installDebian9.sh
@@ -12,7 +12,13 @@ yellow "默认 root 密码是:uMiss233"
 echo "默认镜像地址是:$mirror_addr"
 read -p "是否使用默认镜像?　[y/n]" isDef
 if [[ $isDef != "y" ]];then
-  read -p "请输入镜像地址:" mirror_addr  
+  read -p "请输入镜像地址:" mirror_addr
+  if [[ $mirror_addr =~ http://ftp\.[a-z]+\.debian\.org/debian/ ]];then
+    :
+  else
+    echo "输入的地址有误..."
+    exit 1
+  fi
 fi
 echo "安装过程大约30分钟,根据vps性能不同有所差异..."
 echo "安装好之后,记得修改密码哦!"
