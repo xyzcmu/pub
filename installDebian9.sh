5a30a0a8d7939e273a03cdc18c18e2e6f3695be8
xyzcmu <49193694+xyzcmu@users.noreply.github.com>
2020-07-21 13:26:54 +0800

    Update installDebian9.sh

--- installDebian9.sh
+++ installDebian9.sh
@@ -9,10 +9,10 @@ wget --no-check-certificate -qO InstallNET.sh 'https://moeclub.org/attachment/Li
 mirror_addr="http://ftp.hk.debian.org/debian/"
 
 yellow "默认 root 密码是:uMiss233"
-echo "默认镜像地址是:http://ftp.hk.debian.org/debian/"
+echo "默认镜像地址是:$mirror_addr"
 read -p "是否使用默认镜像?　[y/n]" isDef
 if [[ $isDef != "y" ]];then
-  read -p "请输入镜像地址:" mirror_addr
+  read -p "请输入镜像地址:" mirror_addr  
 fi
 echo "安装过程大约30分钟,根据vps性能不同有所差异..."
 echo "安装好之后,记得修改密码哦!"
