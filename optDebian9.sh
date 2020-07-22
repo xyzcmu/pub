4fa4b3848e90e4ea520d4570a5d48ef055165c19
xyzcmu <49193694+xyzcmu@users.noreply.github.com>
2020-07-22 22:53:27 +0800

    Update optDebian9.sh

--- optDebian9.sh
+++ optDebian9.sh
@@ -18,10 +18,13 @@ commonSet() {
 cat > /etc/apt/sources.list << EOF
 deb http://ftp.us.debian.org/debian/ stretch main
 deb-src http://ftp.us.debian.org/debian/ stretch main
+
 deb http://deb.debian.org/debian/ stretch main
 deb-src http://deb.debian.org/debian/ stretch main
+
 deb http://security.debian.org/ stretch/updates main
 deb-src http://security.debian.org/ stretch/updates main
+
 deb http://deb.debian.org/debian/ stretch-updates main
 deb-src http://deb.debian.org/debian/ stretch-updates main
 EOF
